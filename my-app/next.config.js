8e9134772ba32fb9b4b5022fa672f847fa77268a
Vidhi Munjhwani <vidhimunjwani@gmail.com>
2026-07-24 22:12:58 +0530

    Fix Next config root

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,10 +1,6 @@
-const path = require("node:path");
 const { withAui } = require("@assistant-ui/next");
 
 const nextConfig = {
-  turbopack: {
-    root: __dirname,
-  },
   allowedDevOrigins: ["http://10.38.76.128"],
 };
 
