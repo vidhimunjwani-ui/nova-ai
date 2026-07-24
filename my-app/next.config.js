3c4d0ff499c0c42ce9b98861a668905cfc22901a
Vidhi Munjhwani <vidhimunjwani@gmail.com>
2026-07-24 23:08:17 +0530

    fix: configure Amplify Next.js standalone SSR deployment

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,6 +1,9 @@
+const path = require("path");
 const { withAui } = require("@assistant-ui/next");
 
 const nextConfig = {
+  output: "standalone",
+  outputFileTracingRoot: path.join(__dirname),
   allowedDevOrigins: ["http://10.38.76.128"],
 };
 
